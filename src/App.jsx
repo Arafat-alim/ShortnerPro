cbf65c65fe74ad75939d5e0585918b0f1b034acd
arafat-alim <arafat97alim@gmail.com>
2024-06-27 14:32:03 +0530

    Router: config implemented.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import AppLayout from "./layout/app-layout";
+import Landing from "./pages/landing-page";
+import Dashboard from "./pages/dashboard";
+import Auth from "./pages/auth";
+import Link from "./pages/link";
+import RedirectLink from "./pages/redirect-link";
 
 function App() {
-  return (
-    <>
-      <div>
-        <h1>Welcome back!!</h1>
-      </div>
-    </>
-  );
+  const router = createBrowserRouter([
+    {
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Landing />,
+        },
+        {
+          path: "/dashboard",
+          element: <Dashboard />,
+        },
+        {
+          path: "/auth",
+          element: <Auth />,
+        },
+        {
+          path: "/link/:id",
+          element: <Link />,
+        },
+        {
+          path: "/:id",
+          element: <RedirectLink />,
+        },
+      ],
+    },
+  ]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
