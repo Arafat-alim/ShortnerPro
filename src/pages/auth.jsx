e425f8ebf81980d0439dcf6d5a399307694a9f5d
arafat-alim <arafat97alim@gmail.com>
2024-06-28 18:24:44 +0530

    Frontend: working on authentication page.

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { useSearchParams } from "react-router-dom";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [searcHParams] = useSearchParams();
+
+  return (
+    <div className="mt-36 flex flex-col items-center gap-10">
+      <h1 className="text-5xl font-extrabold">
+        {searcHParams.get("createNew")
+          ? "Hold on! Let's Login first!"
+          : "Login / Signup"}
+      </h1>
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="signup">Signup</TabsTrigger>
+        </TabsList>
+        <TabsContent value="login">Login Component</TabsContent>
+        <TabsContent value="signup">Sign up Component</TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default Auth;
