b46870a505134c6e6cab112f2c43e86051e8434e
arafat-alim <arafat97alim@gmail.com>
2024-06-29 19:20:16 +0530

    feat: Signup function added.

--- src/db/apiAuth.js
+++ src/db/apiAuth.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function login({ email, password }) {
   const { data, error } = await supabase.auth.signInWithPassword({
@@ -10,10 +10,38 @@ export async function login({ email, password }) {
   return data;
 }
 
+//! Get current User
 export async function getCurrentUser() {
   const { data: session, error } = await supabase.auth.getSession();
   if (!session.session) return null;
   if (error) throw new Error(error.message);
 
   return session.session.user;
+}
+
+//! signup
+
+export async function signup({ name, email, password, profile_pic }) {
+  // upload the profile picture
+  const fileName = `dp-${name.split(" ").join("-")}-${Math.random()}`;
+  const { storageError } = await supabase.storage
+    .from("profile_pic")
+    .upload(fileName, profile_pic);
+
+  if (storageError) throw new Error(storageError.message);
+
+  // API calling to upload this image to the supabase storage
+  const { data, signUpError } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        name,
+        profile_pic: `${supabaseUrl}/storage/v1/object/public/profile_pic/${profile_pic}`,
+      },
+    },
+  });
+
+  if (signUpError) throw new Error(signUpError.message);
+  return data;
 }
