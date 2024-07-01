4acdf1d24ce7a8d459313f5a6beecab1a52abc4f
arafat-alim <arafat97alim@gmail.com>
2024-07-01 18:40:50 +0530

    feat: If user is logged in then redirected to dashboard, implemented.

--- src/pages/auth.jsx
+++ src/pages/auth.jsx
@@ -1,17 +1,28 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Login from "@/components/login";
 import Signup from "@/components/signup";
+import { UrlState } from "@/context";
+import { useEffect } from "react";
 
 const Auth = () => {
   const [searcHParams] = useSearchParams();
+  const longUrl = searcHParams.get("createNow");
+
+  //  if user already logged in then dont show this page
+  const navigate = useNavigate();
+  const { isAuthenticated, loading } = UrlState();
+
+  useEffect(() => {
+    if (isAuthenticated && !loading) {
+      navigate(`/dashboard?${longUrl ? `createNew=${longUrl}` : ""}`);
+    }
+  }, [isAuthenticated, loading]);
 
   return (
-    <div className="mt-36 flex flex-col items-center gap-10">
+    <div className="mt-20 flex flex-col items-center gap-10">
       <h1 className="text-5xl font-extrabold">
-        {searcHParams.get("createNew")
-          ? "Hold on! Let's Login first!"
-          : "Login / Signup"}
+        {longUrl ? "Hold on! Let's Login first!" : "Login / Signup"}
       </h1>
       <Tabs defaultValue="login" className="w-[400px]">
         <TabsList className="grid w-full grid-cols-2">
