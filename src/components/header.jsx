392b055c64ebb2e199e7e78f9166dfee3a493140
arafat-alim <arafat97alim@gmail.com>
2024-07-04 23:59:55 +0530

    UI: working on create url UI component.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -68,8 +68,10 @@ const Header = () => {
               <DropdownMenuLabel>{user?.user_metadata?.name}</DropdownMenuLabel>
               <DropdownMenuSeparator />
               <DropdownMenuItem>
-                <LinkIcon className="h-4 w-4 mr-2" />
-                <span>My Links</span>
+                <Link to={"/dashboard"} className="flex">
+                  <LinkIcon className="h-4 w-4 mr-2" />
+                  <span>My Links</span>
+                </Link>
               </DropdownMenuItem>
               <DropdownMenuItem className="text-red-400">
                 <LogOut className="w-4 h-4 mr-2" />
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,3 +1,4 @@
+import CreateLink from "@/components/create-link";
 import Error from "@/components/error";
 import LinkCard from "@/components/link-card";
 import { Button } from "@/components/ui/button";
@@ -74,7 +75,7 @@ const Dashboard = () => {
       </div>
       <div className="flex justify-between">
         <h1 className="text-4xl font-extrabold">My Links</h1>
-        <Button>Create Link</Button>
+        <CreateLink />
       </div>
       {/* rendering the links */}
       <div className="relative">
